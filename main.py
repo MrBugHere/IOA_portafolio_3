3289d9d857d997af83c91b6369d13c3acaf76578
JustMrBug <jose.garcia.z@mail.pucv.cl>
2020-11-08 00:48:37 -0300

    adjacency matrix 2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from casos.c100x500 import data, Vertex_Edges
-
 import numpy as np
-import pandas as pd
 
 adj_matrix = np.zeros((Vertex_Edges[1],Vertex_Edges[1]), dtype = int)
+
 for row in data:
     adj_matrix[row[0]-1][row[1]-1] = row[2]
     adj_matrix[row[1]-1][row[0]-1] = row[2]
+
 
 
 
